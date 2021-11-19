5b718d85c1774dcdbd2fcf8ea1134b421cc9138d
Shahar Linial <shaharlini@gmail.com>
2021-11-19 11:11:30 +0000

    adding new feature

---
+++ new-features/cool-libraries/main.py
@@ -0,0 +1,6 @@
+class Library:
+    def __init__(self, name, version, description, url:
+            self.name = name
+            self.version = version
+            self.description = description
+            self.url = url
